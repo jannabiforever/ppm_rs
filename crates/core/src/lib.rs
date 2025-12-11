d9b2880157ab0bb9d9571186eba17721fa7c55be
jannabiforever <jungini1226@gmail.com>
2025-12-11 16:15:46 +0900

    feat: basic structure of the project

---
+++ crates/core/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod context;
+pub mod errors;
+pub mod models;
+pub mod services;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+mod errors;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "ppm")]
+#[command(about = "Project Protocol Manager (CLI)", long_about = None)]
+pub struct PPMCli {
+    #[command(subcommand)]
+    pub command: PPMCommand,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum PPMCommand {
+    Start {
+        #[arg(short, long)]
+        duration: Option<u32>,
+    },
+}
+
+fn load_config() -> Result<(), errors::PPMCliError> {
+    Ok(())
+}
+
+fn setup() -> Result<(), errors::PPMCliError> {
+    Ok(())
+}
+
+fn main() -> Result<(), errors::PPMCliError> {
+    // Setup
+    load_config()?;
+    setup()?;
+
+    let cli = PPMCli::parse();
+    match cli.command {
+        PPMCommand::Start { duration } => {
+            println!("[ppm] Starting focus session");
+            if let Some(duration) = duration {
+                println!("Duration: {} seconds", duration);
+            } else {
+                println!("Duration: 60 seconds");
+            }
+        }
+    }
+    Ok(())
 }
