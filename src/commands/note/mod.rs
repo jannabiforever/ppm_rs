b2ded5b2a05d98f846f17d476598cda01b202dff
jannabiforever <jungini1226@gmail.com>
2025-12-13 17:29:23 +0900

    rename: add -> new

--- src/commands/note/mod.rs
+++ src/commands/note/mod.rs
@@ -1,13 +1,13 @@
 use clap::Subcommand;
 
-pub mod add;
 pub mod delete;
 pub mod list;
+pub mod new;
 
 #[derive(Debug, Subcommand)]
 pub enum NoteCommand {
 	/// Add a new note
-	New(add::AddCommand),
+	New(new::NewCommand),
 	/// List notes
 	List(list::ListCommand),
 	/// Delete a note
